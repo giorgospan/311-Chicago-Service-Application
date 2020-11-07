e01fe06438464e18ea708ae1917ffe612125212e
George Panagiotopoulos <giorgospan96@gmail.com>
2020-11-08 01:12:35 +0200

    Initial commit for our application

---
+++ src/test/java/com/dit/incidents/ChicagoIncidentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dit.incidents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChicagoIncidentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
