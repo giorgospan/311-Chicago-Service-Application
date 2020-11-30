37fabdcb24df9e180aacb5b43d20ffa0f1231200
George Panagiotopoulos <giorgospan96@gmail.com>
2020-12-01 01:18:50 +0200

    Added components/services - finished login page

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -5,17 +5,29 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {AlertModule} from 'ngx-bootstrap/alert';
 import {HomeComponent} from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegistrationComponent } from './pages/registration/registration.component';
+import { QueryComponent } from './pages/query/query.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     HomeComponent,
+    LoginComponent,
+    RegistrationComponent,
+    QueryComponent,
+    HeaderComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    AlertModule
+    AlertModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
