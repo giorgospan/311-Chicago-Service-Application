4c0ab004d34b34a8523c677df737b67307a23f3f
George Panagiotopoulos <giorgospan96@gmail.com>
2023-03-18 03:18:56 +0200

    Replaced pandas deprecated append with concat

--- parser/import.py
+++ parser/import.py
@@ -3,43 +3,42 @@ import psycopg2
 
 
 def pretty_print(msg):
-    print("="*len(msg))
+    print("=" * len(msg))
     print(msg)
-    print("="*len(msg))
+    print("=" * len(msg))
     print()
 
 
 # Connect to an existing database
-conn = psycopg2.connect("dbname='311CI' user='postgres' host='localhost' password='pass'")
+conn = psycopg2.connect("dbname='311CI' user='postgres' host='localhost' password='postgres'")
 
 # Open a cursor to perform database operations
 cur = conn.cursor()
 
 DIR = './out/'
-input_files = [DIR+f for f in os.listdir(DIR) if f.endswith('.csv')]
+input_files = [DIR + f for f in os.listdir(DIR) if f.endswith('.csv')]
 
 # Add our SQL function + PostGIS extension
 cur.execute(open("extras.sql", "r").read())
 pretty_print('Added SQL function + PostGIS extension')
 
-
 # We need to import the request table first
-fname = DIR+'request.csv'
-with open(fname,'rb') as f:
-    relation = fname.replace(DIR,'').replace('.csv','')
-    cur.execute('TRUNCATE TABLE '+relation+' CASCADE;')
-    SQL = "COPY {} FROM '{}' CSV QUOTE '\"';".format(relation,os.path.abspath(fname))
+fname = DIR + 'request.csv'
+with open(fname, 'rb') as f:
+    relation = fname.replace(DIR, '').replace('.csv', '')
+    cur.execute('TRUNCATE TABLE ' + relation + ' CASCADE;')
+    SQL = "COPY {} FROM '{}' CSV QUOTE '\"';".format(relation, os.path.abspath(fname))
     cur.execute(SQL)
-    pretty_print('Imported ' + fname +  ' to relation ' + relation)
+    pretty_print('Imported ' + fname + ' to relation ' + relation)
 
 # Copy every other csv to the respective PostgreSQL table
-for fname in [x for x in input_files if x != DIR+'request.csv']:
-    with open(fname,'rb') as f:
-        relation = fname.replace(DIR,'').replace('.csv','')
-        cur.execute('TRUNCATE TABLE '+relation+' CASCADE;')
-        SQL = "COPY {} FROM '{}' CSV QUOTE '\"';".format(relation,os.path.abspath(fname))
+for fname in [x for x in input_files if x != DIR + 'request.csv']:
+    with open(fname, 'rb') as f:
+        relation = fname.replace(DIR, '').replace('.csv', '')
+        cur.execute('TRUNCATE TABLE ' + relation + ' CASCADE;')
+        SQL = "COPY {} FROM '{}' CSV QUOTE '\"';".format(relation, os.path.abspath(fname))
         cur.execute(SQL)
-        pretty_print('Imported ' + fname +  ' to relation ' + relation)
+        pretty_print('Imported ' + fname + ' to relation ' + relation)
 
 # Make the changes to the database persistent
 conn.commit()
--- parser/parser.py
+++ parser/parser.py
@@ -3,65 +3,65 @@ import os
 import glob
 import errno
 
+
 def clean(filename):
-
     # Read csv into a df
-    df =  pd.read_csv(INPUT_DIR+filename)
+    df = pd.read_csv(INPUT_DIR + filename)
 
-    print("="*len(filename))
+    print("=" * len(filename))
     print(filename)
-    print("="*len(filename))
+    print("=" * len(filename))
     print()
 
     new_column_names = {
 
         # Common
-        'Street Address' : 'street_address',
-        'Creation Date' : 'creation_date',
-        'Completion Date' : 'completion_date',
-        'Status' : 'status',
-        'Service Request Number' : 'request_number',
-        'Street Address' : 'street_address',
-        'Zip Code' : 'zip_code',
-        'Zip Codes' : 'zip_codes',
-        'Ward' : 'ward',
-        'Wards' : 'wards',
-        'Location' : 'location',
-        'Historical Wards 2003-2015' : 'historical_wards',
-        'Community Area' : 'community_area',
-        'Community Areas' : 'community_areas',
-        'Census Tracts' : 'census_tracts',
-        'Police District' : 'police_district',
-        'Type Of Service Request' : 'request_type',
-        'Latitude' : 'latitude',
-        'Longitude' : 'longitude',
-        'X Coordinate' : 'x_coordinate',
-        'Y Coordinate' : 'y_coordinate',
+        'Street Address': 'street_address',
+        'Creation Date': 'creation_date',
+        'Completion Date': 'completion_date',
+        'Status': 'status',
+        'Service Request Number': 'request_number',
+        'Street Address': 'street_address',
+        'Zip Code': 'zip_code',
+        'Zip Codes': 'zip_codes',
+        'Ward': 'ward',
+        'Wards': 'wards',
+        'Location': 'location',
+        'Historical Wards 2003-2015': 'historical_wards',
+        'Community Area': 'community_area',
+        'Community Areas': 'community_areas',
+        'Census Tracts': 'census_tracts',
+        'Police District': 'police_district',
+        'Type Of Service Request': 'request_type',
+        'Latitude': 'latitude',
+        'Longitude': 'longitude',
+        'X Coordinate': 'x_coordinate',
+        'Y Coordinate': 'y_coordinate',
 
         # RequestType-specific
-        'Current Activity' : 'current_activity',
-        'Most Recent Action' : 'most_recent_action',
-        'Zip':'zip_code',
-        'Ssa' : 'ssa',
-        'License Plate' : 'license_plate',
-        'Vehicle Color' : 'vehicle_color',
-        'Vehicle Make/Model' : 'vehicle_model',
-        'How Many Days Has The Vehicle Been Reported As Parked?' : 'days_parked',
+        'Current Activity': 'current_activity',
+        'Most Recent Action': 'most_recent_action',
+        'Zip': 'zip_code',
+        'Ssa': 'ssa',
+        'License Plate': 'license_plate',
+        'Vehicle Color': 'vehicle_color',
+        'Vehicle Make/Model': 'vehicle_model',
+        'How Many Days Has The Vehicle Been Reported As Parked?': 'days_parked',
         'Number Of Black Carts Delivered': 'num_of_black_carts',
-        'What Type Of Surface Is The Graffiti On?' : 'graffiti_surface',
-        'Where Is The Graffiti Located?' : 'graffiti_location',
-        'Number Of Potholes Filled On Block' : 'potholes_filled',
-        'Number Of Premises Baited' : 'premises_baited',
-        'Number Of Premises With Garbage' : 'premises_with_garbage',
+        'What Type Of Surface Is The Graffiti On?': 'graffiti_surface',
+        'Where Is The Graffiti Located?': 'graffiti_location',
+        'Number Of Potholes Filled On Block': 'potholes_filled',
+        'Number Of Premises Baited': 'premises_baited',
+        'Number Of Premises With Garbage': 'premises_with_garbage',
         'Number Of Premises With Rats': 'premises_with_rats',
-        'What Is The Nature Of This Code Violation?' : 'nature',
-        'If Yes, Where Is The Debris Located?' : 'debris_location',
-        'Location Of Trees' : 'tree_location'
+        'What Is The Nature Of This Code Violation?': 'nature',
+        'If Yes, Where Is The Debris Located?': 'debris_location',
+        'Location Of Trees': 'tree_location'
 
     }
 
     # Output filenames (they correspond to relation names)
-    outnames={
+    outnames = {
         '311-service-requests-abandoned-vehicles.csv': 'vehicle_request.csv',
         '311-service-requests-alley-lights-out.csv': 'alley_lights_request.csv',
         '311-service-requests-garbage-carts.csv': 'garbage_request.csv',
@@ -74,7 +74,6 @@ def clean(filename):
         '311-service-requests-tree-debris.csv': 'tree_debris_request.csv',
         '311-service-requests-tree-trims.csv': 'tree_trim_request.csv'
     }
-
 
     # Columns that exist in all csv files
     common_columns = [
@@ -106,25 +105,24 @@ def clean(filename):
 
     # Rename column names to match column names in our DB
     df.columns = map(str.title, df.columns)
-    df.rename(columns = new_column_names,inplace=True)
+    df.rename(columns=new_column_names, inplace=True)
 
     # Drop location column [redundant]
-    df.drop(columns=['location'],inplace=True)
-
+    df.drop(columns=['location'], inplace=True)
 
     # Remove 'T' from dates
-    df['creation_date'] = df['creation_date'].str.replace('T',' ')
-    df['completion_date'] = df['completion_date'].str.replace('T',' ')
+    df['creation_date'] = df['creation_date'].str.replace('T', ' ')
+    df['completion_date'] = df['completion_date'].str.replace('T', ' ')
 
     # Applies only on './data/311-service-requests-street-lights-one-out.csv'
-    df['request_type'] = df['request_type'].str.replace('Street Light Out','Street Light - 1/Out')
+    df['request_type'] = df['request_type'].str.replace('Street Light Out', 'Street Light - 1/Out')
 
     # Applies only on './data/311-service-requests-pot-holes-reported.csv'
-    df['request_type'] = df['request_type'].str.replace('Pot Hole in Street','Pothole in Street')
+    df['request_type'] = df['request_type'].str.replace('Pot Hole in Street', 'Pothole in Street')
 
     # Filter license plates
     if 'license_plate' in df.columns:
-        df['license_plate']  = df[df['license_plate'].str.len() < 10]['license_plate']
+        df['license_plate'] = df[df['license_plate'].str.len() < 10]['license_plate']
 
     df['longitude'] = df['longitude'].map(lambda x: '%20.17f' % x)
     df['latitude'] = df['latitude'].map(lambda x: '%20.17f' % x)
@@ -134,24 +132,24 @@ def clean(filename):
 
     # Change index
     df.reset_index(drop=True, inplace=True)
-    df.index+=clean.start_pos
+    df.index += clean.start_pos
 
     # Increment starting position for the next call
-    clean.start_pos+=df.shape[0]
+    clean.start_pos += df.shape[0]
 
     # Make index one of the columns with name 'request_id'
-    df['request_id'] = df.index+1
+    df['request_id'] = df.index + 1
 
     # Create csv with specific columns for this request type [i.e.: exclude common columns]
     current_df = df[df.columns[~df.columns.isin([c for c in common_columns if c != 'request_id'])]]
-    current_df = current_df[['request_id']+sorted([a for a in current_df.columns.to_list() if a != 'request_id'])]
-    current_df.to_csv(OUTPUT_DIR+outnames[filename],index=False,header=False,float_format='%.f')
+    current_df = current_df[['request_id'] + sorted([a for a in current_df.columns.to_list() if a != 'request_id'])]
+    current_df.to_csv(OUTPUT_DIR + outnames[filename], index=False, header=False, float_format='%.f')
 
     # Keep only common columns
     df = df[df.columns[df.columns.isin(common_columns)]]
 
     # Append rows to request_df
-    clean.request_df = clean.request_df.append(df)
+    clean.request_df = pd.concat([clean.request_df,df])
 
 
 INPUT_DIR = './data/'
@@ -167,7 +165,7 @@ except OSError as e:
         raise
 
 # Delete all files in OUTPUT_DIR
-for f in glob.glob(OUTPUT_DIR+'*'):
+for f in glob.glob(OUTPUT_DIR + '*'):
     os.remove(f)
 
 # Produce clean csv files
@@ -179,6 +177,6 @@ for f in input_files:
 # Produce general request.csv
 df = clean.request_df
 
-excluded_cols = ['request_id','user_reg_id']
-df = df[['request_id'] + sorted([a for a in df.columns.to_list() if a not in excluded_cols]) +['user_reg_id']]
-df.to_csv(OUTPUT_DIR+'request.csv',header=False,index=False,float_format='%.f')
+excluded_cols = ['request_id', 'user_reg_id']
+df = df[['request_id'] + sorted([a for a in df.columns.to_list() if a not in excluded_cols]) + ['user_reg_id']]
+df.to_csv(OUTPUT_DIR + 'request.csv', header=False, index=False, float_format='%.f')
